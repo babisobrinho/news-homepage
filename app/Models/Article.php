fdb8df9c3398d4ff03f6433567b987ee6f658126
Babi Sobrinho <79843387+babsobrinho@users.noreply.github.com>
2023-03-27 12:22:40 +0100

    add delete image function

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Article extends Model
 {
@@ -16,10 +17,20 @@ class Article extends Model
         'category_id',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($article) {
+            // delete the image file when the article is being deleted
+            Storage::delete('public/' . $article->image);
+        });
+    }
+
     public function scopeNew($query)
-{
-    return $query->orderByDesc('created_at');
-}
+    {
+        return $query->orderByDesc('created_at');
+    }
 
     // one-to-many relationship
     public function user()
